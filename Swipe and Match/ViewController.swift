53fd79140eedc7b31ef093a06007f0e83fc048aa
Victor Mihaita <victormihaita@artlandapp.com>
2019-05-04 00:00:41 +0200

    Initial Setup

--- Swipe and Match/ViewController.swift
+++ Swipe and Match/ViewController.swift
@@ -11,7 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let redView = UIView()
+        redView.backgroundColor = .red
+
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+
+        let stackView = UIStackView(arrangedSubviews: [redView, blueView])
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 
 
